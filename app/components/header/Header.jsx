cd949cb9cedf588b53bb77b883fe72a05f32439b
Mahmoud Ahmed <mahmoudahmed9go@gmail.com>
2023-11-21 10:53:09 +0200

    navbar

--- app/components/header/Header.jsx
+++ app/components/header/Header.jsx
@@ -1,10 +1,38 @@
 import React from 'react';
+import './header.css';
 
 const Header = () => {
     return (
-        <div>
-            <h1>Header</h1>
-        </div>
+        <header className='flex px-6 py-1 items-center justify-between tracking-wide'>
+                <div></div>
+            <nav className='bg-[var(--bgHeader)] py-4 px-8 rounded-full'>
+                <ul className='flex gap-4'>
+                    <li>
+                        <a href="#" className='duration-300'>About</a>
+                    </li>
+                    
+                    <li>
+                        <a href="#" className='duration-300'>Articles</a>
+                    </li>
+                    
+                    <li>
+                        <a href="#" className='duration-300'>Projects</a>
+                    </li>
+                    
+                    <li>
+                        <a href="#" className='duration-300'>Speaking</a>
+                    </li>
+                    
+                    <li>
+                        <a href="#" className='duration-300'>Contact</a>
+                    </li>
+                </ul>
+            </nav>
+
+            <button className=' bg-slate-400 rounded-full px-2 mx-4'>
+                light
+            </button>
+        </header>
     );
 }
 
