24f5474cfbefcae9ae9dc6a1ab48f7b22db43a35
Mahmoud Ahmed <mahmoudahmed9go@gmail.com>
2023-12-03 07:34:58 +0200

    Hero Section Done!

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,5 @@
 import Image from 'next/image'
-import Hero from './components/Hero/Hero'
+import Hero from './components/hero/Hero'
 import Header from './components/header/Header'
 import Main from './components/main/Main'
 import Contact from './components/contact/Contact'
