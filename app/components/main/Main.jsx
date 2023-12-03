64603f5fe5e23c1c25e5953c06ba0d1084164dc2
Mahmoud Ahmed <mahmoudahmed9go@gmail.com>
2023-12-03 10:12:14 +0200

    Button Bar Done!

--- app/components/main/Main.jsx
+++ app/components/main/Main.jsx
@@ -2,8 +2,16 @@ import React from 'react';
 
 const Main = () => {
     return (
-        <div>
-            <h1>Main</h1>
+        <div className='flex gap-12'>
+            <section className='flex flex-col gap-3 text-center'>
+                <button className='bg-[var(--ButtonBG)] py-3 w-44 opacity-75 active:opacity-100 rounded-md hover:opacity-100 duration-300 active:font-bold border border-[var(--blue)]'>All projects</button>
+                <button className='bg-[var(--ButtonBG)] py-3 w-44 opacity-75 active:opacity-100 rounded-md hover:opacity-100 duration-300 active:font-bold border border-[var(--blue)]'>Django</button>
+                <button className='bg-[var(--ButtonBG)] py-3 w-44 opacity-75 active:opacity-100 rounded-md hover:opacity-100 duration-300 active:font-bold border border-[var(--blue)]'>Flask</button>
+                <button className='bg-[var(--ButtonBG)] py-3 w-44 opacity-75 active:opacity-100 rounded-md hover:opacity-100 duration-300 active:font-bold border border-[var(--blue)]'>React js & Tailwind</button>
+                <button className='bg-[var(--ButtonBG)] py-3 w-44 opacity-75 active:opacity-100 rounded-md hover:opacity-100 duration-300 active:font-bold border border-[var(--blue)]'>Fullstack</button>
+                <button className='bg-[var(--ButtonBG)] py-3 w-44 opacity-75 active:opacity-100 rounded-md hover:opacity-100 duration-300 active:font-bold border border-[var(--blue)]'>C Programming</button>
+            </section>
+            <section className='border border-green-500 flex-grow'>Left Section</section>
         </div>
     );
 }
