3831af2250f2c68261d79c1379d36db8072966d0
Mahmoud Ahmed <mahmoudahmed9go@gmail.com>
2023-12-04 14:41:32 +0200

    Footer Done!

--- app/components/footer/Footer.jsx
+++ app/components/footer/Footer.jsx
@@ -1,10 +1,31 @@
 import React from 'react';
+import './footer.css';
 
 const Footer = () => {
     return (
-        <div>
-            <h1>Footer</h1>
-        </div>
+        <footer className='flex justify-between'>
+            <ul className='flex gap-4'>
+
+                <li className=''>
+                    <a href="#">About</a>
+                </li>
+                <li className=''>
+                    <a href="#">Articles</a>
+                </li>
+                <li className=''>
+                    <a href="#">Projects</a>
+                </li>
+                <li className=''>
+                    <a href="#">Speaking</a>
+                </li>
+                <li className=''>
+                    <a href="#">Contact</a>
+                </li>
+                
+            </ul>   
+
+            <p>&copy; 2023 Mahmoud Ahmed. All rights reserved.</p>
+        </footer>
     );
 }
 
