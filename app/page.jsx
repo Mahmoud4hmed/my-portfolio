bde6f5cd30507eb9851422c95944b0fad13409b9
Mahmoud Ahmed <mahmoudahmed9go@gmail.com>
2023-12-06 04:49:56 +0200

    Scroll up button

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,4 @@
+'use client';
 import Image from 'next/image'
 import Hero from './components/hero/Hero'
 import Header from './components/header/Header'
@@ -5,8 +6,22 @@ import Main from './components/main/Main'
 import Contact from './components/contact/Contact'
 import Footer from './components/footer/Footer'
 import Link from 'next/link'
+import { useState, useEffect } from 'react'
 
 export default function Home() {
+
+  const [scroll, setScroll] = useState(false)
+
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      if (window.scrollY > 300) {
+        setScroll(true)
+      } else {
+        setScroll(false)
+      }
+    })
+  }, []);
+
   return (
     <main id='topp' className="w-5/6 bg-[var(--secondary)] p-8 border-solid border-2 border-[var(--border)] mx-auto">
       <Header />
@@ -17,9 +32,12 @@ export default function Home() {
       <Contact />
       <hr className='border-solid border-1 border-[var(--border)] my-12' />
       <Footer />
-      <a href="#topp">
-        <button className='icon-arrow_up text-xl bg-[var(--scrollButton)] w-12 h-12 rounded-full fixed bottom-8 right-[3%]'></button>
-      </a>
+
+      {scroll && 
+        <a href="#topp">
+          <button className='icon-arrow_up text-xl bg-[var(--scrollButton)] w-12 h-12 rounded-full fixed bottom-8 right-[3%]'></button>
+        </a>
+      }
     </main>
   )
 }
